24eb898eeb62af5fcff0815e29feebd718be0d13
hediye-ersan <hediyesnl9@gmail.com>
2024-12-09 17:59:20 +0300

    Home sayfasına fatured product kısmını ekler

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,11 +1,28 @@
 import Footer from '../layout/Footer'
 
+
+
 function HomePage() {
 
     return (
-<div>
-   <Footer/>
-</div>
+        <div>
+            <section>
+                <div>
+                    <h5 className='text-h5 text-bold'>Featured Products</h5>
+                    <h2 className='text-h2'>We love what we do</h2>
+                    <p className='text-h6 text-secondText'>Problems trying to resolve the conflict between the two major realms of Classical physics:
+                        Newtonian mechanics
+
+                        Problems trying to resolve the conflict between the two major realms of Classical physics:
+                        Newtonian mechanics </p>
+                </div>
+                <div className='flex'>
+                    <img src='/images/featured1.png'/>
+                    <img src='/images/featured2.png'/>
+                </div>
+            </section>
+            <Footer />
+        </div>
     );
 }
 export default HomePage;
