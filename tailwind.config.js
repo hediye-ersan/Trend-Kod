f61c98b4b3931c403a600f66f38581805ccc0e8f
hediye-ersan <hediyesnl9@gmail.com>
2024-12-09 14:29:00 +0300

    reset.css oluşturur diğer css dosyalarını düzenler

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#FFFFFF',
+        primaryText:'#252B42'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat','sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
