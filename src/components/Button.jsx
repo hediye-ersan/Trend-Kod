ffba80fd04da64211457bc4d5a277f4949ff8b48
hediye-ersan <hediyesnl9@gmail.com>
2024-12-09 17:58:19 +0300

    button.jsx'e css ekler

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ function Button({text, onClick}) {
     return (
 
         <button
-            className="bg-gradient-to-r from-blue-500 to-cyan-400 text-white font-bold py-3 px-6 rounded-lg shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-200"
+            className="bg-[#23A6F0] text-white  py-4 px-8  transition-transform duration-200"
             onClick={onClick}
         >
             {text}
