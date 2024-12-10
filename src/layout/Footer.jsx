d89cf67f05271b3bc1119f519e82899e7e7b0dd2
hediye-ersan <hediyesnl9@gmail.com>
2024-12-10 22:20:48 +0300

    Sosyal medya iconlarını href ile ekler

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -11,11 +11,16 @@ function Footer() {
         <main className='text-left flex flex-col justify-center'>
             <div className='px-12 py-16 bg-[#FAFAFA] gap-4 flex-col flex'>
                 <h3 className='text-h3 font-bold'>Bendage</h3>
-                <img src="/images/social-image.png" alt="social icons" />
+                <div className='flex gap-6'>
+                    <a href='#'><img src="/icons/facebook.png" alt="Facebook" /></a>
+                    <a href='#'><img src="/icons/insta.png" alt="İnstagram" /></a>
+                    <a href='#'><img src="/icons/twitter.png" alt="Twitter" /></a>
+                </div>
+                
             </div>
-            <article className='flex flex-col gap-8 leading-loose px-12'>
+            <article className='flex flex-col gap-8 leading-loose px-12 font-bold'>
                 <div>
-                    <h5 className='text-h5 font-bold'>Company Info</h5>
+                    <h5 className='text-h5 '>Company Info</h5>
                     <div className='text-h6 text-secondText '>
                         <p>About Us</p>
                         <p>Carrier</p>
@@ -24,7 +29,7 @@ function Footer() {
                     </div>
                 </div>
                 <div>
-                    <h5 className='text-h5 font-bold'>Legal</h5>
+                    <h5 className='text-h5 '>Legal</h5>
                     <div className='text-h6 text-secondText'>
                         <p>About Us</p>
                         <p>Carrier</p>
@@ -33,7 +38,7 @@ function Footer() {
                     </div>
                 </div>
                 <div>
-                    <h5 className='text-h5 font-bold'>Features</h5>
+                    <h5 className='text-h5'>Features</h5>
                     <div className='text-h6 text-secondText'>
                         <p>Business Marketing</p>
                         <p>User Analytic</p>
@@ -57,16 +62,16 @@ function Footer() {
                     <input
                         type="email"
                         placeholder="Your email"
-                        className=" py-2 px-0 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className=" py-1.5 focus:outline-none focus:ring-2 focus:ring-blueText text-secondText pl-4"
                     />
                     <button
                         type="submit"
-                        className="px-6 py-2 bg-blueText text-white rounded-md hover:bg-blue-200"
+                        className="px-6 py-2 bg-blueText text-white rounded-r hover:bg-blue-200"
                     >
                         Subscribe
                     </button>
                 </div>
-                <p className='text-xs text-secondText'>Lore imp sum dolor Amit</p>
+                <p className='text-xs text-secondText font-bold'>Lore imp sum dolor Amit</p>
             </section>
             <div>
                 <h6 className='text-h6 text-secondText bg-[#FAFAFA] h6 px-28 py-10 font-bold text-center'>Made With Love By
