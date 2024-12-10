ac3476208da21ab835c6a96cd8e2ef8576e2841e
hediye-ersan <hediyesnl9@gmail.com>
2024-12-10 15:21:51 +0300

    button css değişiklikleri yapar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ function Button({text, onClick}) {
     return (
 
         <button
-            className="bg-[#23A6F0] text-white  py-4 px-8  transition-transform duration-200"
+            className="bg-blueText text-white  py-4 px-8  transition-transform duration-200"
             onClick={onClick}
         >
             {text}
