ec026462b5b323ceaa12aee5a180b42d1541d873
hediye-ersan <hediyesnl9@gmail.com>
2024-12-11 15:40:57 +0300

    shoopingPage.jsx e gerekli layout ve componenetleri ekler

--- src/page/ShopPage.jsx
+++ src/page/ShopPage.jsx
@@ -1,11 +1,17 @@
-
+import CategoryCard from "../components/CategoryCard";
+import IconList from "../components/IconCard";
+import Footer from "../layout/Footer";
 import Navbar from "../layout/Header";
 
 function ShopPage() {
 
     return (
+        <>
         <Navbar />
-
+        <CategoryCard/>
+        <IconList/>
+        <Footer/>
+        </>
     );
 }
 
