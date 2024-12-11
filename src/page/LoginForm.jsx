a06c5eafe84200646e80f90d85c2fa7225a8a0d8
hediye-ersan <hediyesnl9@gmail.com>
2024-12-11 17:26:38 +0300

    LoginForm örneği oluşturur

---
+++ src/page/LoginForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
