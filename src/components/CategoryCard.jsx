526e9527faf5dd4d7bbc242eb4c27344ba700937
hediye-ersan <hediyesnl9@gmail.com>
2024-12-11 15:40:08 +0300

    CategoryCard için resimler ekler ve yapının bir kısmını oluşturur

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,10 +1,35 @@
-function CategoryCard() {
+function CategoryCard({title, subtitle, image}) {
     return(
         <section>
-            <div>
-                <h3>Shop</h3>
-                
+            <div  className="flex flex-col items-center justify-center py-12">
+                <h3 className="text-h3 font-bold text-center">Shop</h3>
+                <div className="flex gap-2 pt-10">
+                    <h6 className="text-h6">Home</h6>
+                    <img src="/icons/chevron-right.svg"/>
+                    <h6 className="text-h6">Shop</h6>
+                </div>
             </div>
+
+            <div>
+                <div style={{backgroundImage: `url(${image})`}}>
+                    <div>
+                    <h5>{title}</h5>
+                    <h6>{subtitle}</h6>
+                    </div>
+                </div>
+            </div>
+            
         </section>
     );
 }
+const CategoryList =() => {
+const images= [
+    './images/Category1.png',
+    './images/Category2.png',
+    './images/Category3.png',
+    './images/Category4.png',
+    './images/Category5.png',
+]
+}
+
+export default CategoryCard;
