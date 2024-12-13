0681499745e9e799bd4fe4100553acc4de9e3a9c
hediye-ersan <hediyesnl9@gmail.com>
2024-12-13 11:06:15 +0300

    Css stilleri revize eder ve arka plan resimleri ekler

--- src/page/SignupForm.jsx
+++ src/page/SignupForm.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import "../reset.css";
 
 // Axios ile verilen URL ile bağlantı kuruluyor
 const api = axios.create({
@@ -94,20 +95,26 @@ const SignupForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="signup-form space-y-4 p-6 bg-gray-50 rounded-lg shadow-lg">
-      {errors.api && <p className="text-red-600 text-sm">{errors.api.message}</p>}
-      <h2 className="text-xl font-bold text-gray-800 mb-4">Sign In</h2>
+    <div className="flex h-screen">
+    {/* Görsel Bölümü */}
+    <div
+      className="w-1/3 bg-cover bg-center rounded-l-lg"
+      style={{ backgroundImage: "url('./images/Signupbg.png')" }}
+    ></div>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-2/3 bg-white flex flex-col p-8 justify-center rounded-r-lg">
+      {errors.api && <p className="text-red-600 text-h6">{errors.api.message}</p>}
+      <h2 className="text-h2 font-bold ">Sign In</h2>
       <div className="flex flex-col">
-        <label className="font-medium text-gray-700">Name</label>
-        <input className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500" id="name"
+        <label className="font-medium text-secondText">Name</label>
+        <input className="border border-[#BDBDBD] bg-[#F9F9F9] rounded-md p-2  " id="name"
           {...register('name', { required: true, minLength: 3 })} />
-        <p className="text-red-600 text-sm">{errors.name?.message}</p>
+        <p className="text-red-600 text-h6">{errors.name?.message}</p>
       </div>
 
       <div className="flex flex-col">
-        <label className="font-medium text-gray-700">Email</label>
+        <label className="font-medium text-secondText">Email</label>
         <input
-          className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+          className="border border-[#BDBDBD] rounded-md p-2 "
           id="email"
           type="email"
           {...register('email', {
@@ -115,39 +122,39 @@ const SignupForm = () => {
             pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
           })}
         />
-        <p className="text-red-600 text-sm">{errors.email?.message}</p>
+        <p className="text-red-600 text-h6">{errors.email?.message}</p>
       </div>
 
       <div className="flex flex-col">
-        <label className="font-medium text-gray-700">Password</label>
-        <input className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500" id="password"
+        <label className="font-medium text-secondText">Password</label>
+        <input className="border border-[#BDBDBD] rounded-md p-2 " id="password"
           type="password"
           {...register('password', {
             required: true,
             minLength: 8,
             pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/
           })} />
-        <p className="text-red-600 text-sm">{errors.password?.message}</p>
+        <p className="text-red-600 text-h6">{errors.password?.message}</p>
       </div>
 
       <div className="flex flex-col">
-        <label className="font-medium text-gray-700">Confirm Password</label>
+        <label className="font-medium text-secondText">Confirm Password</label>
         <input
-          className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+          className="border border-[#BDBDBD] rounded-md p-2 "
           id="confirmPassword"
           type="password"
           {...register('confirmPassword', {
             validate: (value) => value === watch('password') || "Passwords do not match"
           })}
         />
-        <p className="text-red-600 text-sm">{errors.confirmPassword?.message}</p>
+        <p className="text-red-600 text-h6">{errors.confirmPassword?.message}</p>
       </div>
 
       {/* Role Selection */}
       <div className="flex flex-col">
-        <label className="font-medium text-gray-700">Role</label>
+        <label className="font-medium text-secondText">Role</label>
         <select
-          className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+          className="border border-[#BDBDBD] rounded-md p-2 text-secondText"
           {...register("role_id", { required: "Role selection is required" })}
           onChange={(e) => setSelectedRole(e.target.value)}
           value={selectedRole}
@@ -158,24 +165,24 @@ const SignupForm = () => {
             </option>
           ))}
         </select>
-        <p className="text-red-600 text-sm">{errors.role_id?.message}</p>
+        <p className="text-red-600 text-h6">{errors.role_id?.message}</p>
       </div>
 
       {/* Store Fields (If "Store" is selected) */}
       {selectedRole === "store" && (
         <>
           <div className="flex flex-col">
-            <label className="font-medium text-gray-700">Store Name</label>
+            <label className="font-medium text-secondText">Store Name</label>
             <input
-              className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+              className="border border-[#BDBDBD] rounded-md p-2 "
               {...register("store_name", { required: "Store name is required", minLength: 3 })}
             />
-            <p className="text-red-600 text-sm">{errors.store_name?.message}</p>
+            <p className="text-red-600 text-h6">{errors.store_name?.message}</p>
           </div>
           <div className="flex flex-col">
-            <label className="font-medium text-gray-700">Store Phone</label>
+            <label className="font-medium text-secondText">Store Phone</label>
             <input
-              className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+              className="border border-[#BDBDBD] rounded-md p-2 "
               {...register("store_phone", {
                 required: "Phone number is required",
                 pattern: {
@@ -184,12 +191,12 @@ const SignupForm = () => {
                 },
               })}
             />
-            <p className="text-red-600 text-sm">{errors.store_phone?.message}</p>
+            <p className="text-red-600 text-h6">{errors.store_phone?.message}</p>
           </div>
           <div className="flex flex-col">
-            <label className="font-medium text-gray-700">Tax ID</label>
+            <label className="font-medium text-secondText">Tax ID</label>
             <input
-              className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+              className="border border-[#BDBDBD] rounded-md p-2"
               {...register("tax_no", {
                 required: "Tax ID is required",
                 pattern: {
@@ -198,12 +205,12 @@ const SignupForm = () => {
                 },
               })}
             />
-            <p className="text-red-600 text-sm">{errors.tax_no?.message}</p>
+            <p className="text-red-600 text-h6">{errors.tax_no?.message}</p>
           </div>
           <div className="flex flex-col">
-            <label className="font-medium text-gray-700">Bank Account</label>
+            <label className="font-medium text-secondText">Bank Account</label>
             <input
-              className="border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+              className="border border-[#BDBDBD] rounded-md p-2"
               {...register("bank_account", {
                 required: "Bank Account is required",
                 pattern: {
@@ -212,7 +219,7 @@ const SignupForm = () => {
                 },
               })}
             />
-            <p className="text-red-600 text-sm">{errors.bank_account?.message}</p>
+            <p className="text-red-600 text-h6">{errors.bank_account?.message}</p>
           </div>
         </>
       )}
@@ -221,11 +228,12 @@ const SignupForm = () => {
       <button
         type="submit"
         disabled={loading}
-        className={`w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 ${loading ? "opacity-50 cursor-not-allowed" : ""}`}
+        className={`w-full bg-blueText text-white  font-bold py-2 mt-2 mb-6 px-4 rounded-md hover:bg-blue-500 ${loading ? "opacity-50 cursor-not-allowed" : ""}`}
       >
         {loading ? "Loading..." : "Sign Up"}
       </button>
     </form>
+    </div>
   );
 };
 
