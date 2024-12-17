6a2cb3c5d866de4f3ca005e3e11569106b6cdb55
hediye-ersan <hediyesnl9@gmail.com>
2024-12-17 11:46:40 +0300

    store ya userReducer ekler

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,14 +2,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import {thunk} from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import clientReducer from '../reducers/clientReducer';
+import userReducer from '../reducers/userReducer';
 import productReducer from '../reducers/productReducer';
 import shoppingCartReducer from '../reducers/shoppinCartReducer';
 
 const logger = createLogger();
 
 const rootReducer = combineReducers({
-    client: clientReducer, 
+    user: userReducer,
     product: productReducer,
     shoppingCart: shoppingCartReducer,
 });
