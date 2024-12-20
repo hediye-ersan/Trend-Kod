e003b26f04c6c14de6f7534a2540771a3abb1985
hediye-ersan <hediyesnl9@gmail.com>
2024-12-20 11:34:55 +0300

    ProductDetail Sayfasına ProductCard componenetini ekler

--- src/page/ProductDetails.jsx
+++ src/page/ProductDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProductCard from '../components/ProductDetailCard';
 
 function ProductDetails() {
     return (
         <div>
             <h1>Product Details</h1>
+            <ProductCard/>
         </div>
     )
 }
