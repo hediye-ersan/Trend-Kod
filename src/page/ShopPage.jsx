49d1c263396464025c4eed6b6f14e9ce2c0927d2
hediye-ersan <hediyesnl9@gmail.com>
2024-12-23 14:42:57 +0300

    ProductCardList.jsx e product bilgilerini props olarak iletir

--- src/page/ShopPage.jsx
+++ src/page/ShopPage.jsx
@@ -1,16 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from "../actions/productsActions";
 import FashionCategories from "../components/CategoryCard";
 import IconList from "../components/IconCard";
-import ProductCardList from "../components/ProductCard";
+import ProductCardList from "../components/ProductCardList";
 import Footer from "../layout/Footer";
 import Navbar from "../layout/Header";
 
+
 function ShopPage() {
+    const dispatch = useDispatch();
+  const { products, loading, error } = useSelector(state => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  
 
     return (
         <>
         <Navbar />
         <FashionCategories/>
-        <ProductCardList/>
+        <ProductCardList products={products}/>
         <IconList/>
         <Footer/>
         </>
