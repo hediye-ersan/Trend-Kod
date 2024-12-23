c82fcfdba307324bbbd111e3aee2915e5838ecd4
hediye-ersan <hediyesnl9@gmail.com>
2024-12-23 14:43:32 +0300

    combine reducer'a productReducer ekler

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,9 +1,9 @@
 // store.js
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import {thunk} from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import userReducer from '../reducers/userReducer';
-import productReducer from '../reducers/productReducer';
+import {productReducer} from '../reducers/productReducer';
 import shoppingCartReducer from '../reducers/shoppinCartReducer';
 import categoriesReducer from '../reducers/categoriesReducer';
 
@@ -11,9 +11,10 @@ const logger = createLogger();
 
 const rootReducer = combineReducers({
     user: userReducer,
-    product: productReducer,
     shoppingCart: shoppingCartReducer,
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    products: productReducer
+
 
 });
 
