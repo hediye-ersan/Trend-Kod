6ec99073c78a06f435d89d67419fe5b2013388fa
hediye-ersan <hediyesnl9@gmail.com>
2024-12-28 16:24:15 +0300

    productDetails için reducer ekler

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,31 +1,38 @@
 const initialState = {
-    products: [],
-    total: 0,
-    loading: false,
-    error: null
-  };
-  
-  export const productReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'FETCH_PRODUCTS_REQUEST':
-        return {
-          ...state,
-          loading: true
-        };
-      case 'FETCH_PRODUCTS_SUCCESS':
-        return {
-          ...state,
-          loading: false,
-          products: action.payload.products,
-          total: action.payload.total
-        };
-      case 'FETCH_PRODUCTS_FAILURE':
-        return {
-          ...state,
-          loading: false,
-          error: action.payload
-        };
-      default:
-        return state;
-    }
-  };
+  products: [],
+  total: 0,
+  loading: false,
+  error: null,
+  productDetails: null,
+};
+
+export const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_PRODUCTS_REQUEST':
+      return {
+        ...state,
+        loading: true
+      };
+    case 'FETCH_PRODUCTS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        products: action.payload.products,
+        total: action.payload.total
+      };
+    case 'FETCH_PRODUCTS_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    case 'FETCH_PRODUCT_DETAILS':
+      return {
+        ...state,
+        loading: false,
+        productDetails: action.payload, // Store product details
+      };
+    default:
+      return state;
+  }
+};
