d4f828b10a574253eb57d29137bbc7dfcc81b07d
hediye-ersan <hediyesnl9@gmail.com>
2024-12-28 16:25:43 +0300

    ürünün diğer bilgilerini ekler

--- src/components/ProductCardList.jsx
+++ src/components/ProductCardList.jsx
@@ -5,22 +5,28 @@ import ProductCard from './ProductCard';
 const ProductCardList = ({ products = [] }) => {
   return (
     <>
-    
-    <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {products.map((product, index) => (
-          <ProductCard
-            key={index}
-            id={product.id}
-            title={product.name}
-            subtitle={product.description}
-            price={product.price}
-            discountedPrice={product.discountedPrice || product.price} // Assuming discountedPrice is optional
-            image={product.images[0]?.url} // Assuming the first image is the main image
-          />
-        ))}
+
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              stock={product.stock}
+              store_id={product.store_id}
+              category_id={product.category_id}
+              rating={product.rating}
+              sell_count={product.sell_count}
+              discountedPrice={product.discountedPrice || product.price}
+              image={product.images[0]?.url}
+                     
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
