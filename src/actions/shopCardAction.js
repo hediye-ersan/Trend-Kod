634f907c8e8dc7a31bff67d3f454107590312aa6
hediye-ersan <hediyesnl9@gmail.com>
2024-12-30 12:40:59 +0300

    ekleme-çıkarma actionlarını ekler ve reducerda kullanır

--- src/actions/shopCardAction.js
+++ src/actions/shopCardAction.js
@@ -2,6 +2,7 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const INCREASE_PRODUCT_QUANTITY = 'INCREASE_PRODUCT_QUANTITY';
+export const DECREASE_PRODUCT_QUANTITY = 'DECREASE_PRODUCT_QUANTITY';
 
 // Alışveriş sepetine ürün ekleme eylemi
 export const addToCart = (product) => ({
@@ -17,4 +18,9 @@ export const removeFromCart = (productId) => ({
 export const increaseProductQuantity = (productId) => ({
   type: INCREASE_PRODUCT_QUANTITY,
   payload: productId,
+});
+
+export const decreaseProductQuantity = (productId) => ({
+    type: 'DECREASE_PRODUCT_QUANTITY',
+    payload: productId,
 });
--- src/reducers/shopCardReducer.js
+++ src/reducers/shopCardReducer.js
@@ -1,5 +1,5 @@
 // reducers/cartReducer.js
-import { ADD_TO_CART, REMOVE_FROM_CART, INCREASE_PRODUCT_QUANTITY } from '../actions/shopCardAction';
+import { ADD_TO_CART, REMOVE_FROM_CART, INCREASE_PRODUCT_QUANTITY, DECREASE_PRODUCT_QUANTITY } from '../actions/shopCardAction';
 
 const initialState = {
   cartItems: [],
@@ -7,7 +7,7 @@ const initialState = {
 
 const shopCardReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case ADD_TO_CART:
       const item = action.payload;
       const existingItem = state.cartItems.find(x => x.id === item.id);
       if (existingItem) {
@@ -21,6 +21,22 @@ const shopCardReducer = (state = initialState, action) => {
       return {
         ...state,
         cartItems: [...state.cartItems, {...item, quantity: 1}],
+      };
+
+    case INCREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map(x => 
+          x.id === action.payload ? {...x, quantity: x.quantity + 1} : x
+        ),
+      };
+
+    case DECREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map(x => 
+          x.id === action.payload && x.quantity > 1 ? {...x, quantity: x.quantity - 1} : x
+        ),
       };
 
     case REMOVE_FROM_CART:
