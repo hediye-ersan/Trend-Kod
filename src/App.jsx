7b55a31dd35fe09e20f4524534a93d602648b318
hediye-ersan <hediyesnl9@gmail.com>
2024-12-30 12:40:16 +0300

    alışveriş sepeti sayfasını app.jsx e ekler

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ShopPage from './page/ShopPage'
 import LoginForm from './page/LoginForm'
 import SignupForm from './page/SignupForm'
 import ProductDetails from './page/ProductDetails'
+import ShoppingCartPage from './page/ShoppingCartPage'
 import { ToastContainer } from 'react-toastify';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -12,30 +13,30 @@ import store from './Store/store';
 
 function App() {
 
-
   return (
     <>
       <Provider store={store}>
         <ToastContainer />
-        <Switch>
-          <Route path="/productdetails/:productId">
-            <ProductDetails />
-          </Route>
-          <Route path="/shop">
-            <ShopPage />
-          </Route>
-          <Route path="/signup">
-            <SignupForm />
-          </Route>
-          <Route path="/login">
-            <LoginForm />
-          </Route>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          
-
-        </Switch>
+          <Switch>
+            <Route path="/productdetails/:productId">
+              <ProductDetails />
+            </Route>
+            <Route path="/shop">
+              <ShopPage />
+            </Route>
+            <Route path="/signup">
+              <SignupForm />
+            </Route>
+            <Route path="/login">
+              <LoginForm />
+            </Route>
+            <Route path="/shopping-cart">
+              <ShoppingCartPage />
+            </Route>  
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </Switch>
       </Provider>
     </>
   )
