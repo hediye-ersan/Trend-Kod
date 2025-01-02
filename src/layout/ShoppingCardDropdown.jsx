a8e62048ac15245252192d8b3e86074d3ba3408b
hediye-ersan <hediyesnl9@gmail.com>
2025-01-02 12:55:00 +0300

    revize eder

--- src/layout/ShoppingCardDropdown.jsx
+++ src/layout/ShoppingCardDropdown.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, increaseProductQuantity } from '../actions/shopCardAction';
+import { removeFromCart} from '../actions/shopCardAction';
 import { Link } from 'react-router-dom';
 
 const ShoppingCartDropdown = () => {
