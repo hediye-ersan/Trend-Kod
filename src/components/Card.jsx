1cd91cfcff8e3383b4b5f7390787545c17f7a511
hediye-ersan <hediyesnl9@gmail.com>
2025-01-04 01:04:25 +0300

    desktop için stillendirme ekler

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,52 +1,70 @@
 import React from 'react';
 
 const Card = ({ imageSrc, title, buttonText }) => {
-    return (
-        <div className="relative overflow-hidden ">
-            <img src={imageSrc} alt={title} />
-            <div className="bg-[#2D8BC0BF] bg-opacity-70 flex flex-col items-start justify-center text-white  p-4 absolute bottom-0 left-0  h-2/5 ">
-                <h3 className="text-h3 font-bold mb-2 text-left">{title}</h3>
-                <div>
-                    <button className="px-4 py-2 bg-transparent rounded border border-white border-1 hover:text-white transition duration-300">
-                        {buttonText}
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative overflow-hidden h-full">
+      <img
+        src={imageSrc}
+        alt={title}
+        className="w-full h-full object-cover"
+      />
+      <div className="bg-[#2D8BC0BF] bg-opacity-70 flex flex-col items-start justify-center text-white p-6 absolute bottom-0 left-0 h-2/5 w-full">
+        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <button className="px-4 py-2 bg-transparent rounded border border-white hover:bg-white hover:text-[#2D8BC0] transition duration-300">
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 const CardList = () => {
-    const cards = [
-        {
-            imageSrc: './images/card1.png',
-            title: 'Top Product Of the Week',
-            buttonText: 'Explore Items',
-        },
-        {
-            imageSrc: './images/card2.png',
-            title: 'Top Product Of the Week',
-            buttonText: 'Explore Items',
-        },
-        {
-            imageSrc: './images/card3.png',
-            title: 'Top Product Of the Week',
-            buttonText: 'Explore Items',
-        },
-    ];
+  const cards = [
+    {
+      imageSrc: './images/card1.png',
+      title: 'Top Product Of the Week',
+      buttonText: 'Explore Items',
+    },
+    {
+      imageSrc: './images/card2.png',
+      title: 'Top Product Of the Week',
+      buttonText: 'Explore Items',
+    },
+    {
+      imageSrc: './images/card3.png',
+      title: 'Top Product Of the Week',
+      buttonText: 'Explore Items',
+    },
+  ];
 
-    return (
-        <section className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center sm:grid-cols-2 sm:grid-rows-2 ">
-            {cards.map((card, index) => (
-                <Card
-                    key={index}
-                    imageSrc={card.imageSrc}
-                    title={card.title}
-                    buttonText={card.buttonText}
-                />
-            ))}
-        </section>
-    );
-}
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-3 gap-4 md:grid-rows-2 h-screen">
+      {/* Sol geniş kart */}
+      <div className="md:row-span-2 md:col-span-2">
+        <Card
+          imageSrc={cards[0].imageSrc}
+          title={cards[0].title}
+          buttonText={cards[0].buttonText}
+        />
+      </div>
+
+      {/* Sağdaki küçük kartlar */}
+      <div className="md:col-span-1">
+        <Card
+          imageSrc={cards[1].imageSrc}
+          title={cards[1].title}
+          buttonText={cards[1].buttonText}
+        />
+      </div>
+      <div className="md:col-span-1">
+        <Card
+          imageSrc={cards[2].imageSrc}
+          title={cards[2].title}
+          buttonText={cards[2].buttonText}
+        />
+      </div>
+    </section>
+  );
+};
+
 export default CardList;
-
