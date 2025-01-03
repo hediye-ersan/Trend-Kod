02cf342201fe5518cd38a14bde5a5d92c0087fec
hediye-ersan <hediyesnl9@gmail.com>
2025-01-03 18:22:11 +0300

    Siparişleri listelemek için reducer ekler

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -1,7 +1,10 @@
 import {
     CREATE_ORDER_START,
     CREATE_ORDER_SUCCESS,
-    CREATE_ORDER_FAIL
+    CREATE_ORDER_FAIL,
+    FETCH_ORDERS_START,
+    FETCH_ORDERS_SUCCESS,
+    FETCH_ORDERS_FAIL
 } from '../actions/orderActions';
 
 const initialState = {
@@ -22,9 +25,27 @@ const orderReducer = (state = initialState, action) => {
             return {
                 ...state,
                 isLoading: false,
-                orders: [...state.orders, action.payload]
+                orders: [...state.orders, action.payload] // Ensure new order is added to the orders array
             };
         case CREATE_ORDER_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            };
+        case FETCH_ORDERS_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: null
+            };
+        case FETCH_ORDERS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                orders: action.payload
+            };
+        case FETCH_ORDERS_FAIL:
             return {
                 ...state,
                 isLoading: false,
